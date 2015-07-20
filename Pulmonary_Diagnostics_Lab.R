7fd6752ccbdba39f51dc9db0e6b026fa1df2a8d0
dwjack <darbyjack@gmail.com>
2015-07-20 11:07:49 +0000

    commented code

--- Pulmonary_Diagnostics_Lab.R
+++ Pulmonary_Diagnostics_Lab.R
@@ -1,11 +1,21 @@
+# script for reading Pulmonary Diagnostics data into R
+
+# replace with correct file path
 file <- "~/Desktop/Desktop/desktop/temp/DJ_bike_study_dry_run_2.txt"
 
+# read the data in to a dataframe called pdl
   
 pdl <- read.table(file, skip = 12, fill = TRUE, allowEscapes = FALSE,   row.names = NULL )
 
-pdl2 <- pdl[complete.cases(pdl),]
+# note that this reads in the header information from each page
+# the following line drops that information
+
+pdl <- pdl[complete.cases(pdl),]
 
 names(pdl) <- c("time", "work", "VO2/kg", "VE", "Vt", "RR", "RQ", "HR", "level")
+
+# add a column for rider ID
+
 
 head(pdl)
 
